737d63408b982aa5264cffd0e718a2be4f4ec9f2
ardagurpinar <arda.gurpinar2001@gmail.com>
2025-05-18 15:58:45 +0300

    removed UserRepository method

--- src/main/java/com/ardagurpinar/sports_clash/config/AppConfig.java
+++ src/main/java/com/ardagurpinar/sports_clash/config/AppConfig.java
@@ -7,5 +7,5 @@ import org.springframework.context.annotation.Configuration;
 @Configuration
 public class AppConfig {
     @Bean
-    ModelMapper modelMapper() { return new ModelMapper(); };
+    ModelMapper modelMapper() { return new ModelMapper(); }
 }
